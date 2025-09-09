9f29a70837deaaf6f87d1f9fc7ee65bbc1f05716
Mamoro85 <angel_sos_2002@mail.ru>
2025-09-09 21:24:10 +0300

    Update pkg/db/task.go
    Co-authored-by: Ivan Nizhnichenko <ivan_0805@mail.ru>

--- pkg/db/task.go
+++ pkg/db/task.go
@@ -65,7 +65,7 @@ func GetTask(id string) (*Task, error) {
 	query := `SELECT id, date, title, comment, repeat FROM scheduler WHERE id = ?`
 	err := DB.QueryRow(query, id).Scan(&task.ID, &task.Date, &task.Title, &comment, &repeat)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return nil, fmt.Errorf("Задача не найдена")
 		}
 		return nil, fmt.Errorf("ошибка получения задачи: %w", err)
